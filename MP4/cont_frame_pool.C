8cafade706f3db7dc718cb5aa9382ef7d3d04674
Sorush Virtual Lab <Sorush.Omidvar@tamu.edu>
2021-10-21 13:36:21 -0500

    MP4:Major:copying the code from the previous MPs.

--- MP4/cont_frame_pool.C
+++ MP4/cont_frame_pool.C
@@ -94,7 +94,8 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-/* -- (none) -- */
+#define MB * (0x1 << 20)
+#define KB * (0x1 << 10)
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
@@ -104,6 +105,7 @@
 #include "console.H"
 #include "utils.H"
 #include "assert.H"
+
 
 /*--------------------------------------------------------------------------*/
 /* DATA STRUCTURES */
@@ -126,37 +128,282 @@
 /*--------------------------------------------------------------------------*/
 /* METHODS FOR CLASS   C o n t F r a m e P o o l */
 /*--------------------------------------------------------------------------*/
+int ContFramePool::PoolCounter=0;
+ContFramePool* ContFramePool::Pools[] ={};
 
 ContFramePool::ContFramePool(unsigned long _base_frame_no,
-                             unsigned long _nframes,
+                             unsigned long _n_frames,
                              unsigned long _info_frame_no,
                              unsigned long _n_info_frames)
 {
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+    BaseAdd = _base_frame_no;//512 for kernel which is located at 2 MB
+    FrameNum = _n_frames;//512 which translates into 2 MB located between 2MB and 4 MB
+    InfoAdd = _info_frame_no;//Location of the manager
+    InfoNum = _n_info_frames;//Number of frames required for the frame manager
+
+    //To avoid fragmentation we should ensure that we have 4 page per char
+    assert ((FrameNum % 4 ) == 0);
+    
+    // If _info_frame_no is zero then we keep management info in the first
+    //frame, else we use the provided frame to keep management info
+    if(InfoAdd == 0) {
+        assert (FrameNum*FRAME_SIZE<=(2 MB));// to assure that it is maximum of 2 MB for Kernel frame pool
+        assert (BaseAdd*FRAME_SIZE <= (4 MB)); //to assure the pool starts before 4 MB for kernel frame pool
+        assert (BaseAdd*FRAME_SIZE >= (2 MB)); //to assure the pool ends after 4 MB for kernel frame pool
+
+        Bitmap = (unsigned char *) (BaseAdd * FRAME_SIZE); // this is the frame manager for only the kernel
+    } else {
+        assert (FrameNum*FRAME_SIZE<=(28 MB));// to assure that it is maximum of 28 MB for proccess frame pool
+        assert (BaseAdd*FRAME_SIZE <= (32 MB)); //to assure the pool starts before 32 MB for process frame pool
+        assert (BaseAdd*FRAME_SIZE >= (4 MB)); //to assure the pool ends after 4 MB for process frame pool
+
+        Bitmap = (unsigned char *) (InfoAdd * FRAME_SIZE); //this is the frame manager for only the process
+    }
+    
+    //Initiliziation which makes all the bitmap 1 and all the head 0
+    for(int i=0; i< FrameNum/4; i++) {
+        FreeFrameNum+=4;
+        Bitmap[i] = 85; //where the even bits are availibility and odd ones are the head
+    }
+
+    // Mark the first frame as being used if it is being used and as a head
+    if(InfoAdd == 0) {//this is the kernel frame manager
+        Bitmap[0] = 86;//making the first frame unavailaible as it holds kernel frame manager and 
+        FreeFrameNum--;
+    }
+
+    //printinting the details of the pool
+    Console::puts("Base=");Console::putui(BaseAdd);Console::puts(" Frame#=");Console::putui(FrameNum);
+    Console::puts(" Info=");Console::putui(InfoAdd);Console::puts(" Info#=");Console::putui(InfoNum);
+    Console::puts(" Free=");Console::putui(FreeFrameNum);Console::puts(" Pool#=");Console::putui(PoolCounter);
+    Console::puts("\n");
+    
+    //keeping the track of the pools
+    Pools[PoolCounter]= this;
+    PoolCounter++;
 }
 
-unsigned long ContFramePool::get_frames(unsigned int _n_frames)
-{
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+unsigned long ContFramePool::get_frames(unsigned int _n_frames){
+    unsigned long requestedFrameNum = _n_frames;//number of frames needed for the process or kernel
+    bool foundFlag=false;   
+    unsigned long moreFrame;
+    unsigned long bitCounter=0;
+    unsigned long baseSeq=0;
+    
+    while(true){//finding a segment which can host the frames
+        int charPage=int(bitCounter/8);
+        int charRemainder=bitCounter%8;
+
+        int dataBits[8];
+        bitExt(Bitmap[charPage],dataBits);
+
+        for(int j=charRemainder;j<8;j+=2){
+            if(dataBits[j]==0){
+                moreFrame=requestedFrameNum;
+                baseSeq=bitCounter+2;
+                break;
+            }
+            else{
+                moreFrame--;
+                if(moreFrame==0){
+                    foundFlag=true;
+                    break;
+                }
+            }
+        }
+        if(foundFlag)
+            break;//successfully found a base address to host the data
+        if(bitCounter==FrameNum)
+            return 0;//unsuccessful operation,there is no fram available
+        bitCounter+=2;
+    }
+
+    if(foundFlag){
+        baseSeq=baseSeq/2;//the base addres for the sequence is the address of the bit rather than the frame which is fixed by dividing it by 2. Also, we have to add the base add of the pool
+        baseSeq+=BaseAdd;
+        mark_inaccessible(baseSeq,requestedFrameNum);//reserving the frames
+        Console::puts("ContframePool::get_frames.");
+        if(InfoAdd==0)
+            Console::puts("KERNEL");
+        else
+            Console::puts("PROCESS");
+        Console::puts(" need ");
+        Console::putui(requestedFrameNum);
+        Console::puts(" frames which starts at=");
+        Console::putui(baseSeq);
+        Console::puts("\n");
+        return baseSeq;
+    }
+    Console::puts("ContframePool::There is not enough frame for such a request with the size=");
+    Console::putui(requestedFrameNum);
+    Console::puts("\n");
+    return 0; //reserved value for the situation that the getFrame cannot be fulfilled
 }
 
-void ContFramePool::mark_inaccessible(unsigned long _base_frame_no,
-                                      unsigned long _n_frames)
-{
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+void ContFramePool::mark_inaccessible(unsigned long _base_frame_no,unsigned long _n_frames){
+    unsigned long baseFrameNum=_base_frame_no;
+    baseFrameNum-=BaseAdd;//removing the base address so it matches the bitmap
+    unsigned long frameNum=_n_frames;
+    unsigned long moreFrame=frameNum;
+    unsigned long bitCounter=baseFrameNum*2;
+
+    while(true){//updataing the avaialibility and heads
+        unsigned long charPage=int(bitCounter/8);
+        unsigned long charRemainder=bitCounter%8;
+        
+        int dataBits[8];
+        bitExt(Bitmap[charPage],dataBits);
+
+        int tempEnd=charRemainder+2*moreFrame;
+        if (tempEnd>8)
+            tempEnd=8;
+        for(int counter=charRemainder;counter<tempEnd;counter+=2){
+            dataBits[counter]=0;//making the bit unavailable
+            dataBits[counter+1]=0;//removing previous heads
+            moreFrame--;
+            bitCounter+=2;
+        }
+        Bitmap[charPage]=decimalExt(dataBits);
+        if (moreFrame==0)
+            break;
+    }
+    bitCounter=baseFrameNum*2;
+    {//updating the head
+        unsigned long charPage=int(bitCounter/8);
+        unsigned long charRemainder=bitCounter%8;
+        
+        int dataBits[8];
+        bitExt(Bitmap[charPage],dataBits);
+        dataBits[charRemainder+1]=1;//only one head is needed for the whole section
+        Bitmap[charPage]=decimalExt(dataBits);
+    }
+    FreeFrameNum-=frameNum;
+    Console::puts("ContframePool::mark_inaccessible, base=");
+    Console::putui(baseFrameNum);
+    Console::puts(" and frame number=");
+    Console::putui(frameNum);
+    Console::puts("\n");
 }
 
-void ContFramePool::release_frames(unsigned long _first_frame_no)
-{
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+void ContFramePool::release_frames(unsigned long _first_frame_no){
+    unsigned long frameNum=_first_frame_no;
+    ContFramePool* currentPool=NULL;
+    for(int i=0;i<100;i++){
+        if(Pools[i]!=NULL){
+            if (Pools[i]->BaseAdd<=frameNum && Pools[i]->BaseAdd+Pools[i]->FrameNum>frameNum){
+                currentPool=Pools[i];
+                break;
+            }
+        }
+    }
+
+    if(!currentPool){
+        Console::puts("ContframePool::release_frames could not locate the owner pool. MAYDAY\n");
+        assert(false);
+    }
+
+    unsigned char* currentBitmap=currentPool->Bitmap;
+    unsigned long firstHead=(frameNum-currentPool->BaseAdd)*2+1;
+    unsigned long secondHead=firstHead+2;
+
+    while(true){
+        unsigned long charPage=int(secondHead/8);
+        unsigned long charRemainder=secondHead%8;
+
+        int dataBits[8];
+        for (int i = 0 ; i != 8 ; i++) {
+            dataBits[i] = (currentBitmap[charPage] & (1 << i)) != 0;
+        }
+        if(dataBits[charRemainder]==1)//next head is found
+            break;
+        if(dataBits[charRemainder-1]==1){//empty bit
+            break;
+        }   
+        secondHead+=2;
+        if(secondHead>=currentPool->FrameNum){
+            Console::puts("ContframePool::headFiner-Warning, head is at end of pool!\n");
+            assert(false);
+        }
+    }
+
+    {//updating availibility
+        unsigned long tempBegin=firstHead-1;
+        unsigned long tempEnd=-1;
+        unsigned long moreFrame=((secondHead-1)-(firstHead-1))/2; 
+        while(true){
+            int charPage=int(tempBegin/8);
+            int charRemainder=tempBegin%8;
+            int dataBits[8];
+            for (int bitCounter = 0 ; bitCounter != 8 ; bitCounter++) {
+                dataBits[bitCounter] = (currentBitmap[charPage] & (1 << bitCounter)) != 0;
+            }
+            if(charRemainder+2*moreFrame>=8)
+                tempEnd=8;
+            else
+                tempEnd=charRemainder+2*moreFrame;
+            for(int i=charRemainder;i<tempEnd;i+=2){
+                dataBits[i]=1;//updating the availibility bit
+                dataBits[i+1]=0;//updating the head
+                moreFrame--;
+                tempBegin+=2;
+            }
+            int tempVal=0;
+            int pow=1;
+            for(int i=0;i<8;i++){
+                tempVal+=pow*dataBits[i];
+                pow*=2;
+            }
+            currentBitmap[charPage]=tempVal;
+            if(moreFrame==0)
+                break;
+        }
+    }
+    {//updating head
+        int charPage=int(firstHead/8);
+        int charRemainder=firstHead%8;
+        int dataBits[8];
+        for (int bitCounter = 0 ; bitCounter != 8 ; bitCounter++) {
+            dataBits[bitCounter] = (currentBitmap[charPage] & (1 << bitCounter)) != 0;
+        }
+        dataBits[charRemainder]=0;
+        int tempVal=0;
+        int pow=1;
+        for(int i=0;i<8;i++){
+            tempVal+=pow*dataBits[i];
+            pow*=2;
+        }
+        currentBitmap[charPage]=tempVal;
+    
+    }
+    Console::puts("ContframePool::release_frames released from frame ");
+    Console::putui(currentPool->BaseAdd+(firstHead-1)/2);
+    Console::puts(" till ");
+    Console::putui(currentPool->BaseAdd+(secondHead-1)/2);
+    Console::puts("\n");
 }
 
-unsigned long ContFramePool::needed_info_frames(unsigned long _n_frames)
-{
-    // TODO: IMPLEMENTATION NEEEDED!
-    assert(false);
+unsigned long ContFramePool::needed_info_frames(unsigned long _n_frames){
+    unsigned long infoFrameNum=_n_frames;
+    unsigned long neededInfoFrame=int(infoFrameNum/FRAME_SIZE/4);
+    if(infoFrameNum%(FRAME_SIZE*4)!=0)
+        neededInfoFrame+=1;
+    Console::puts("ContframePool::need_info_frames is working beautifully. Neede info frame=");
+    Console::putui(neededInfoFrame);
+    Console::puts("\n");
+    return neededInfoFrame;
+}
+void ContFramePool::bitExt(unsigned char input,int* dataBits){
+    for (int i = 0 ; i != 8 ; i++) {
+        dataBits[i] = (input & (1 << i)) != 0;
+    }
+}
+
+int ContFramePool::decimalExt(int* dataBits){
+    int tempVal=0;
+    int pow=1;
+    for(int i=0;i<8;i++){
+        tempVal+=pow * dataBits[i];
+        pow*=2;
+    }
+    return tempVal;
 }
--- MP4/kernel.C
+++ MP4/kernel.C
@@ -11,14 +11,13 @@
 
 */
 
-
 /*--------------------------------------------------------------------------*/
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-#define GB * (0x1 << 30)
-#define MB * (0x1 << 20)
-#define KB * (0x1 << 10)
+#define GB *(0x1 << 30)
+#define MB *(0x1 << 20)
+#define KB *(0x1 << 10)
 #define KERNEL_POOL_START_FRAME ((2 MB) / Machine::PAGE_SIZE)
 #define KERNEL_POOL_SIZE ((2 MB) / Machine::PAGE_SIZE)
 #define PROCESS_POOL_START_FRAME ((4 MB) / Machine::PAGE_SIZE)
@@ -32,26 +31,24 @@
 #define FAULT_ADDR (4 MB)
 /* used in the code later as address referenced to cause page faults. */
 #define NACCESS ((1 MB) / 4)
-/* NACCESS integer access (i.e. 4 bytes in each access) are made starting at address FAULT_ADDR */
+/* NACCESS integer access (i.e. 4 bytes in each access) are made starting at
+ * address FAULT_ADDR */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
 
-#include "machine.H"        /* LOW-LEVEL STUFF */
 #include "console.H"
-#include "gdt.H"
-#include "idt.H"            /* LOW-LEVEL EXCEPTION MGMT. */
-#include "irq.H"
 #include "exceptions.H"
+#include "gdt.H"
+#include "idt.H" /* LOW-LEVEL EXCEPTION MGMT. */
 #include "interrupts.H"
-
-#include "simple_keyboard.H" /* SIMPLE KB DRIVER */
-#include "simple_timer.H"   /* SIMPLE TIMER MANAGEMENT */
-
+#include "irq.H"
+#include "machine.H" /* LOW-LEVEL STUFF */
 #include "page_table.H"
 #include "paging_low.H"
-
+#include "simple_keyboard.H" /* SIMPLE KB DRIVER */
+#include "simple_timer.H"    /* SIMPLE TIMER MANAGEMENT */
 #include "vm_pool.H"
 
 /*--------------------------------------------------------------------------*/
@@ -61,7 +58,8 @@
 void TestPassed();
 void TestFailed();
 
-void GeneratePageTableMemoryReferences(unsigned long start_address, int n_references);
+void GeneratePageTableMemoryReferences(unsigned long start_address,
+                                       int n_references);
 void GenerateVMPoolMemoryReferences(VMPool *pool, int size1, int size2);
 
 /*--------------------------------------------------------------------------*/
@@ -74,27 +72,23 @@ VMPool *current_pool;
 
 typedef long unsigned int size_t;
 
-//replace the operator "new"
-void * operator new (size_t size) {
-  unsigned long a = current_pool->allocate((unsigned long)size);
-  return (void *)a;
+// replace the operator "new"
+void *operator new(size_t size) {
+    unsigned long a = current_pool->allocate((unsigned long)size);
+    return (void *)a;
 }
 
-//replace the operator "new[]"
-void * operator new[] (size_t size) {
-  unsigned long a = current_pool->allocate((unsigned long)size);
-  return (void *)a;
+// replace the operator "new[]"
+void *operator new[](size_t size) {
+    unsigned long a = current_pool->allocate((unsigned long)size);
+    return (void *)a;
 }
 
-//replace the operator "delete"
-void operator delete (void * p) {
-  current_pool->release((unsigned long)p);
-}
+// replace the operator "delete"
+void operator delete(void *p) { current_pool->release((unsigned long)p); }
 
-//replace the operator "delete[]"
-void operator delete[] (void * p) {
-  current_pool->release((unsigned long)p);
-}
+// replace the operator "delete[]"
+void operator delete[](void *p) { current_pool->release((unsigned long)p); }
 
 /*--------------------------------------------------------------------------*/
 /* EXCEPTION HANDLERS */
@@ -102,84 +96,79 @@ void operator delete[] (void * p) {
 
 /* -- EXAMPLE OF THE DIVISION-BY-ZERO HANDLER */
 
-void dbz_handler(REGS * r) {
-  Console::puts("DIVISION BY ZERO\n");
-  for(;;);
+void dbz_handler(REGS *r) {
+    Console::puts("DIVISION BY ZERO\n");
+    for (;;)
+        ;
 }
-
 
 /*--------------------------------------------------------------------------*/
 /* MAIN ENTRY INTO THE OS */
 /*--------------------------------------------------------------------------*/
 
 int main() {
-
-   GDT::init();
+    GDT::init();
     Console::init();
     IDT::init();
     ExceptionHandler::init_dispatcher();
     IRQ::init();
     InterruptHandler::init_dispatcher();
 
-    /* -- SEND OUTPUT TO TERMINAL -- */ 
+    /* -- SEND OUTPUT TO TERMINAL -- */
     Console::output_redirection(true);
 
     /* -- EXAMPLE OF AN EXCEPTION HANDLER -- */
-    
+
     class DBZ_Handler : public ExceptionHandler {
-      /* We derive Division-by-Zero handler from ExceptionHandler 
-         and overload the method handle_exception. */
-    public:
-        virtual void handle_exception(REGS * _regs) {
+        /* We derive Division-by-Zero handler from ExceptionHandler
+             and overload the method handle_exception. */
+       public:
+        virtual void handle_exception(REGS *_regs) {
             Console::puts("DIVISION BY ZERO!\n");
-            for(;;);
+            for (;;)
+                ;
         }
     } dbz_handler;
-    
-    /* Register the DBZ handler for exception no.0 
-       with the exception dispatcher. */
+
+    /* Register the DBZ handler for exception no.0
+         with the exception dispatcher. */
     ExceptionHandler::register_handler(0, &dbz_handler);
-    
 
     /* -- INITIALIZE THE TIMER (we use a very simple timer).-- */
-    
+
     SimpleTimer timer(100); /* timer ticks every 10ms. */
-    
-    /* ---- Register timer handler for interrupt no.0 
-            with the interrupt dispatcher. */
+
+    /* ---- Register timer handler for interrupt no.0
+              with the interrupt dispatcher. */
     InterruptHandler::register_handler(0, &timer);
-    
+
     /* NOTE: The timer chip starts periodically firing as
-     soon as we enable interrupts.
-     It is important to install a timer handler, as we
-     would get a lot of uncaptured interrupts otherwise. */
-    
+       soon as we enable interrupts.
+       It is important to install a timer handler, as we
+       would get a lot of uncaptured interrupts otherwise. */
+
     /* -- INSTALL KEYBOARD HANDLER -- */
     SimpleKeyboard::init();
 
     Console::puts("after installing keyboard handler\n");
 
     /* -- ENABLE INTERRUPTS -- */
-    
+
     Machine::enable_interrupts();
 
     /* -- INITIALIZE FRAME POOLS -- */
 
-    ContFramePool kernel_mem_pool(KERNEL_POOL_START_FRAME,
-                                  KERNEL_POOL_SIZE,
-                                  0,
-				  0);
+    ContFramePool kernel_mem_pool(KERNEL_POOL_START_FRAME, KERNEL_POOL_SIZE, 0,
+                                  0);
 
-    unsigned long n_info_frames = 
-      ContFramePool::needed_info_frames(PROCESS_POOL_SIZE);
+    unsigned long n_info_frames =
+        ContFramePool::needed_info_frames(PROCESS_POOL_SIZE);
 
-    unsigned long process_mem_pool_info_frame = 
-      kernel_mem_pool.get_frames(n_info_frames);
+    unsigned long process_mem_pool_info_frame =
+        kernel_mem_pool.get_frames(n_info_frames);
 
-    ContFramePool process_mem_pool(PROCESS_POOL_START_FRAME,
-                                   PROCESS_POOL_SIZE,
-                                   process_mem_pool_info_frame,
-				   n_info_frames);
+    ContFramePool process_mem_pool(PROCESS_POOL_START_FRAME, PROCESS_POOL_SIZE,
+                                   process_mem_pool_info_frame, n_info_frames);
 
     /* Take care of the hole in the memory. */
     process_mem_pool.mark_inaccessible(MEM_HOLE_START_FRAME, MEM_HOLE_SIZE);
@@ -189,23 +178,21 @@ int main() {
     /* ---- INSTALL PAGE FAULT HANDLER -- */
 
     class PageFault_Handler : public ExceptionHandler {
-      /* We derive the page fault handler from ExceptionHandler 
-	 and overload the method handle_exception. */
-      public:
-      virtual void handle_exception(REGS * _regs) {
-        PageTable::handle_fault(_regs);
-      }
+        /* We derive the page fault handler from ExceptionHandler
+             and overload the method handle_exception. */
+       public:
+        virtual void handle_exception(REGS *_regs) {
+            PageTable::handle_fault(_regs);
+        }
     } pagefault_handler;
 
     /* ---- Register the page fault handler for exception no. 14
-            with the exception dispatcher. */
+              with the exception dispatcher. */
     ExceptionHandler::register_handler(14, &pagefault_handler);
 
     /* ---- INITIALIZE THE PAGE TABLE -- */
 
-    PageTable::init_paging(&kernel_mem_pool,
-                           &process_mem_pool,
-                           4 MB);
+    PageTable::init_paging(&kernel_mem_pool, &process_mem_pool, 4 MB);
 
     PageTable pt1;
 
@@ -220,8 +207,8 @@ int main() {
     Console::puts("Hello World!\n");
 
     /* BY DEFAULT WE TEST THE PAGE TABLE IN MAPPED MEMORY!
-       (COMMENT OUT THE FOLLOWING LINE TO TEST THE VM Pools! */
-#define _TEST_PAGE_TABLE_
+         (COMMENT OUT THE FOLLOWING LINE TO TEST THE VM Pools! */
+    // #define _TEST_PAGE_TABLE_
 
 #ifdef _TEST_PAGE_TABLE_
 
@@ -234,12 +221,13 @@ int main() {
 
     /* -- CREATE THE VM POOLS. */
 
-    /* ---- We define the code pool to be a 256MB segment starting at virtual address 512MB -- */
+    /* ---- We define the code pool to be a 256MB segment starting at virtual
+     * address 512MB -- */
     VMPool code_pool(512 MB, 256 MB, &process_mem_pool, &pt1);
 
     /* ---- We define a 256MB heap that starts at 1GB in virtual memory. -- */
     VMPool heap_pool(1 GB, 256 MB, &process_mem_pool, &pt1);
-    
+
     /* -- NOW THE POOLS HAVE BEEN CREATED. */
 
     Console::puts("VM Pools successfully created!\n");
@@ -259,51 +247,54 @@ int main() {
     TestPassed();
 }
 
-void GeneratePageTableMemoryReferences(unsigned long start_address, int n_references) {
-  // This tests just the page table. 
-  int *foo = (int *) start_address;
-  
-  for (int i=0; i<n_references; i++) {
-    foo[i] = i;
-  }
-  
-  Console::puts("DONE WRITING TO MEMORY. Now testing...\n");
+void GeneratePageTableMemoryReferences(unsigned long start_address,
+                                       int n_references) {
+    // This tests just the page table.
+    int *foo = (int *)start_address;
 
-  for (int i=0; i<n_references; i++) {
-    if(foo[i] != i) {
-      TestFailed();
+    for (int i = 0; i < n_references; i++) {
+        foo[i] = i;
     }
-  }
+
+    Console::puts("DONE WRITING TO MEMORY. Now testing...\n");
+
+    for (int i = 0; i < n_references; i++) {
+        if (foo[i] != i) {
+            TestFailed();
+        }
+    }
 }
 
 void GenerateVMPoolMemoryReferences(VMPool *pool, int size1, int size2) {
-  // Here we test the VMPool 
-   current_pool = pool;
-   for(int i=1; i<size1; i++) {
-      int *arr = new int[size2 * i];
-      if(pool->is_legitimate((unsigned long)arr) == false) {
-         TestFailed();
-      }
-      for(int j=0; j<size2*i; j++) {
-         arr[j] = j;
-      }
-      for(int j=size2*i - 1; j>=0; j--) {
-         if(arr[j] != j) {
+    // Here we test the VMPool
+    current_pool = pool;
+    for (int i = 1; i < size1; i++) {
+        int *arr = new int[size2 * i];
+        if (pool->is_legitimate((unsigned long)arr) == false) {
             TestFailed();
-         }
-      }
-      delete[] arr;
-   }
+        }
+        for (int j = 0; j < size2 * i; j++) {
+            arr[j] = j;
+        }
+        for (int j = size2 * i - 1; j >= 0; j--) {
+            if (arr[j] != j) {
+                TestFailed();
+            }
+        }
+        delete[] arr;
+    }
 }
 
 void TestFailed() {
-   Console::puts("Test Failed\n");
-   Console::puts("YOU CAN TURN OFF THE MACHINE NOW.\n");
-   for(;;);
+    Console::puts("Test Failed\n");
+    Console::puts("YOU CAN TURN OFF THE MACHINE NOW.\n");
+    for (;;)
+        ;
 }
 
 void TestPassed() {
-   Console::puts("Test Passed! Congratulations!\n");
-   Console::puts("YOU CAN SAFELY TURN OFF THE MACHINE NOW.\n");
-   for(;;);
+    Console::puts("Test Passed! Congratulations!\n");
+    Console::puts("YOU CAN SAFELY TURN OFF THE MACHINE NOW.\n");
+    for (;;)
+        ;
 }
