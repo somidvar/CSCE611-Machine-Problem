472c08cada287aecf80b4ec792f5a81ac6a2c2b9
Sorush Virtual Lab <Sorush.Omidvar@tamu.edu>
2021-11-25 14:43:21 -0600

    MP6:Major:MP is done

--- MP6/blocking_disk.C
+++ MP6/blocking_disk.C
@@ -12,37 +12,43 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
 
-#include "assert.H"
-#include "utils.H"
-#include "console.H"
 #include "blocking_disk.H"
 
 /*--------------------------------------------------------------------------*/
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
-  : SimpleDisk(_disk_id, _size) {
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size)
+    : SimpleDisk(_disk_id, _size) {
 }
 
 /*--------------------------------------------------------------------------*/
 /* SIMPLE_DISK FUNCTIONS */
 /*--------------------------------------------------------------------------*/
 
-void BlockingDisk::read(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::read(_block_no, _buf);
-
+void BlockingDisk::read(unsigned long _block_no, unsigned char* _buf) {
+    // SimpleDisk::read(_block_no, _buf);
+    SimpleDisk::issue_operation(DISK_OPERATION::READ, _block_no);
+    while (!SimpleDisk::is_ready()) {
+        Scheduler::currentScheduler->add(Thread::CurrentThread());
+        Scheduler::currentScheduler->yield();
+    }
+    SimpleDisk::bufGetter(_buf);
 }
 
-
-void BlockingDisk::write(unsigned long _block_no, unsigned char * _buf) {
-  // -- REPLACE THIS!!!
-  SimpleDisk::write(_block_no, _buf);
+void BlockingDisk::write(unsigned long _block_no, unsigned char* _buf) {
+    // SimpleDisk::write(_block_no, _buf);
+    
+    SimpleDisk::issue_operation(DISK_OPERATION::WRITE, _block_no);
+    while (!SimpleDisk::is_ready()) {
+        Scheduler::currentScheduler->add(Thread::CurrentThread());
+        Scheduler::currentScheduler->yield();
+    }
+    SimpleDisk::bufSetter(_buf);
 }
--- MP6/kernel.C
+++ MP6/kernel.C
@@ -19,72 +19,70 @@
 
 /* -- COMMENT/UNCOMMENT THE FOLLOWING LINE TO EXCLUDE/INCLUDE SCHEDULER CODE */
 
-//#define _USES_SCHEDULER_
+#define _USES_SCHEDULER_
 /* This macro is defined when we want to force the code below to use 
    a scheduler.
    Otherwise, no scheduler is used, and the threads pass control to each 
    other in a co-routine fashion.
 */
 
-#define MB * (0x1 << 20)
-#define KB * (0x1 << 10)
+#define MB *(0x1 << 20)
+#define KB *(0x1 << 10)
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
 
-#include "machine.H"         /* LOW-LEVEL STUFF   */
+#include "blocking_disk.H"
 #include "console.H"
+#include "exceptions.H"
+#include "frame_pool.H" /* MEMORY MANAGEMENT */
 #include "gdt.H"
-#include "idt.H"             /* EXCEPTION MGMT.   */
-#include "irq.H"
-#include "exceptions.H"     
+#include "idt.H" /* EXCEPTION MGMT.   */
 #include "interrupts.H"
-
-#include "simple_timer.H"    /* TIMER MANAGEMENT  */
-
-#include "frame_pool.H"      /* MEMORY MANAGEMENT */
+#include "irq.H"
+#include "machine.H" /* LOW-LEVEL STUFF   */
 #include "mem_pool.H"
-
-#include "thread.H"         /* THREAD MANAGEMENT */
+#include "simple_timer.H" /* TIMER MANAGEMENT  */
+#include "thread.H"       /* THREAD MANAGEMENT */
 
 #ifdef _USES_SCHEDULER_
-#include "scheduler.H"      /* WE WILL NEED A SCHEDULER WITH BlockingDisk */
+#include "scheduler.H" /* WE WILL NEED A SCHEDULER WITH BlockingDisk */
 #endif
 
-#include "simple_disk.H"    /* DISK DEVICE */
-                            /* YOU MAY NEED TO INCLUDE blocking_disk.H
+#include "simple_disk.H" /* DISK DEVICE */
+                         /* YOU MAY NEED TO INCLUDE blocking_disk.H
 /*--------------------------------------------------------------------------*/
 /* MEMORY MANAGEMENT */
 /*--------------------------------------------------------------------------*/
 
 /* -- A POOL OF FRAMES FOR THE SYSTEM TO USE */
-FramePool * SYSTEM_FRAME_POOL;
+FramePool *SYSTEM_FRAME_POOL;
 
 /* -- A POOL OF CONTIGUOUS MEMORY FOR THE SYSTEM TO USE */
-MemPool * MEMORY_POOL;
+MemPool *MEMORY_POOL;
 
 typedef long unsigned int size_t;
 
 //replace the operator "new"
-void * operator new (size_t size) {
+void *operator new(size_t size) {
     unsigned long a = MEMORY_POOL->allocate((unsigned long)size);
     return (void *)a;
 }
 
 //replace the operator "new[]"
-void * operator new[] (size_t size) {
+void *operator new[](size_t size) {
     unsigned long a = MEMORY_POOL->allocate((unsigned long)size);
     return (void *)a;
 }
 
 //replace the operator "delete"
-void operator delete (void * p) {
+void operator delete(void *p) {
     MEMORY_POOL->release((unsigned long)p);
 }
 
 //replace the operator "delete[]"
-void operator delete[] (void * p) {
+void operator delete[](void *p) {
     MEMORY_POOL->release((unsigned long)p);
 }
 
@@ -95,7 +93,7 @@ void operator delete[] (void * p) {
 #ifdef _USES_SCHEDULER_
 
 /* -- A POINTER TO THE SYSTEM SCHEDULER */
-Scheduler * SYSTEM_SCHEDULER;
+Scheduler *SYSTEM_SCHEDULER;
 
 #endif
 
@@ -104,7 +102,7 @@ Scheduler * SYSTEM_SCHEDULER;
 /*--------------------------------------------------------------------------*/
 
 /* -- A POINTER TO THE SYSTEM DISK */
-SimpleDisk * SYSTEM_DISK;
+BlockingDisk *SYSTEM_DISK;
 
 #define SYSTEM_DISK_SIZE (10 MB)
 
@@ -114,22 +112,21 @@ SimpleDisk * SYSTEM_DISK;
 /* JUST AN AUXILIARY FUNCTION */
 /*--------------------------------------------------------------------------*/
 
-void pass_on_CPU(Thread * _to_thread) {
-
+void pass_on_CPU(Thread *_to_thread) {
 #ifndef _USES_SCHEDULER_
 
-        /* We don't use a scheduler. Explicitely pass control to the next
+    /* We don't use a scheduler. Explicitely pass control to the next
            thread in a co-routine fashion. */
-	Thread::dispatch_to(_to_thread); 
+    Thread::dispatch_to(_to_thread);
 
 #else
 
-        /* We use a scheduler. Instead of dispatching to the next thread,
+    /* We use a scheduler. Instead of dispatching to the next thread,
            we pre-empt the current thread by putting it onto the ready
            queue and yielding the CPU. */
 
-        SYSTEM_SCHEDULER->resume(Thread::CurrentThread()); 
-        SYSTEM_SCHEDULER->yield();
+    SYSTEM_SCHEDULER->resume(Thread::CurrentThread());
+    SYSTEM_SCHEDULER->yield();
 #endif
 }
 
@@ -137,93 +134,105 @@ void pass_on_CPU(Thread * _to_thread) {
 /* A FEW THREADS (pointer to TCB's and thread functions) */
 /*--------------------------------------------------------------------------*/
 
-Thread * thread1;
-Thread * thread2;
-Thread * thread3;
-Thread * thread4;
+Thread *thread1;
+Thread *thread2;
+Thread *thread3;
+Thread *thread4;
 
 void fun1() {
-    Console::puts("THREAD: "); Console::puti(Thread::CurrentThread()->ThreadId()); Console::puts("\n");
+    Console::puts("THREAD: ");
+    Console::puti(Thread::CurrentThread()->ThreadId());
+    Console::puts("\n");
 
     Console::puts("FUN 1 INVOKED!\n");
 
-    for(int j = 0;; j++) {
+    for (int j = 0;; j++) {
+        Console::puts("FUN 1 IN ITERATION[");
+        Console::puti(j);
+        Console::puts("]\n");
 
-       Console::puts("FUN 1 IN ITERATION["); Console::puti(j); Console::puts("]\n");
+        for (int i = 0; i < 10; i++) {
+            Console::puts("FUN 1: TICK [");
+            Console::puti(i);
+            Console::puts("]\n");
+        }
 
-       for (int i = 0; i < 10; i++) {
-           Console::puts("FUN 1: TICK ["); Console::puti(i); Console::puts("]\n");
-       }
-
-       pass_on_CPU(thread2);
+        pass_on_CPU(thread2);
     }
 }
 
-
-
 void fun2() {
-    Console::puts("THREAD: "); Console::puti(Thread::CurrentThread()->ThreadId()); Console::puts("\n");
+    Console::puts("THREAD: ");
+    Console::puti(Thread::CurrentThread()->ThreadId());
+    Console::puts("\n");
 
     Console::puts("FUN 2 INVOKED!\n");
 
     unsigned char buf[DISK_BLOCK_SIZE];
-    int  read_block  = 1;
-    int  write_block = 0;
+    int read_block = 1;
+    int write_block = 1;
 
-    for(int j = 0;; j++) {
+    for (int j = 0;; j++) {
+        /* -- Read */
+        Console::puts("Reading a block from disk...\n");
+        SYSTEM_DISK->read(read_block, buf);
 
-       Console::puts("FUN 2 IN ITERATION["); Console::puti(j); Console::puts("]\n");
+        /* -- Display */
+        for (int i = 0; i < DISK_BLOCK_SIZE; i++) {
+            Console::putch(buf[i]);
+        }
 
-       /* -- Read */
-       Console::puts("Reading a block from disk...\n");
-       SYSTEM_DISK->read(read_block, buf);
+        Console::puts("Writing a block to disk...\n");
+        SYSTEM_DISK->write(write_block, buf);
 
-       /* -- Display */
-       for (int i = 0; i < DISK_BLOCK_SIZE; i++) {
-           Console::putch(buf[i]);
-       }
+        /* -- Move to next block */
+        write_block = read_block;
+        read_block = (read_block + 1) % 10;
 
-       Console::puts("Writing a block to disk...\n");
-       SYSTEM_DISK->write(write_block, buf); 
-
-       /* -- Move to next block */
-       write_block = read_block;
-       read_block  = (read_block + 1) % 10;
-
-       /* -- Give up the CPU */
-       pass_on_CPU(thread3);
+        /* -- Give up the CPU */
+        pass_on_CPU(thread3);
     }
 }
 
 void fun3() {
-    Console::puts("THREAD: "); Console::puti(Thread::CurrentThread()->ThreadId()); Console::puts("\n");
+    Console::puts("THREAD: ");
+    Console::puti(Thread::CurrentThread()->ThreadId());
+    Console::puts("\n");
 
     Console::puts("FUN 3 INVOKED!\n");
 
-     for(int j = 0;; j++) {
+    for (int j = 0;; j++) {
+        Console::puts("FUN 3 IN BURST[");
+        Console::puti(j);
+        Console::puts("]\n");
 
-       Console::puts("FUN 3 IN BURST["); Console::puti(j); Console::puts("]\n");
+        for (int i = 0; i < 10; i++) {
+            Console::puts("FUN 3: TICK [");
+            Console::puti(i);
+            Console::puts("]\n");
+        }
 
-       for (int i = 0; i < 10; i++) {
-           Console::puts("FUN 3: TICK ["); Console::puti(i); Console::puts("]\n");
-       }
-    
-       pass_on_CPU(thread4);
+        pass_on_CPU(thread4);
     }
 }
 
 void fun4() {
-    Console::puts("THREAD: "); Console::puti(Thread::CurrentThread()->ThreadId()); Console::puts("\n");
+    Console::puts("THREAD: ");
+    Console::puti(Thread::CurrentThread()->ThreadId());
+    Console::puts("\n");
 
-    for(int j = 0;; j++) {
+    for (int j = 0;; j++) {
+        Console::puts("FUN 4 IN BURST[");
+        Console::puti(j);
+        Console::puts("]\n");
 
-       Console::puts("FUN 4 IN BURST["); Console::puti(j); Console::puts("]\n");
+        for (int i = 0; i < 10; i++) {
+            Console::puts("FUN 4: TICK [");
+            Console::puti(i);
+            Console::puts("]\n");
+        }
 
-       for (int i = 0; i < 10; i++) {
-           Console::puts("FUN 4: TICK ["); Console::puti(i); Console::puts("]\n");
-       }
-
-       pass_on_CPU(thread1);
+        pass_on_CPU(thread1);
     }
 }
 
@@ -232,7 +241,6 @@ void fun4() {
 /*--------------------------------------------------------------------------*/
 
 int main() {
-
     GDT::init();
     Console::init();
     IDT::init();
@@ -240,17 +248,18 @@ int main() {
     IRQ::init();
     InterruptHandler::init_dispatcher();
 
-     /* -- SEND OUTPUT TO TERMINAL -- */ 
+    /* -- SEND OUTPUT TO TERMINAL -- */
     Console::output_redirection(true);
 
     /* -- EXAMPLE OF AN EXCEPTION HANDLER -- */
 
     class DBZ_Handler : public ExceptionHandler {
-      public:
-      virtual void handle_exception(REGS * _regs) {
-        Console::puts("DIVISION BY ZERO!\n");
-        for(;;);
-      }
+       public:
+        virtual void handle_exception(REGS *_regs) {
+            Console::puts("DIVISION BY ZERO!\n");
+            for (;;)
+                ;
+        }
     } dbz_handler;
 
     ExceptionHandler::register_handler(0, &dbz_handler);
@@ -263,7 +272,7 @@ int main() {
     /* ---- Initialize a frame pool; details are in its implementation */
     FramePool system_frame_pool;
     SYSTEM_FRAME_POOL = &system_frame_pool;
-   
+
     /* ---- Create a memory pool of 256 frames. */
     MemPool memory_pool(SYSTEM_FRAME_POOL, 256);
     MEMORY_POOL = &memory_pool;
@@ -283,23 +292,23 @@ int main() {
 #ifdef _USES_SCHEDULER_
 
     /* -- SCHEDULER -- IF YOU HAVE ONE -- */
-  
+
     SYSTEM_SCHEDULER = new Scheduler();
 
 #endif
 
     /* -- DISK DEVICE -- */
 
-    SYSTEM_DISK = new SimpleDisk(DISK_ID::MASTER, SYSTEM_DISK_SIZE);
-   
+    SYSTEM_DISK = new BlockingDisk(DISK_ID::MASTER, SYSTEM_DISK_SIZE);
+
     /* NOTE: The timer chip starts periodically firing as 
              soon as we enable interrupts.
              It is important to install a timer handler, as we 
-             would get a lot of uncaptured interrupts otherwise. */  
+             would get a lot of uncaptured interrupts otherwise. */
 
     /* -- ENABLE INTERRUPTS -- */
 
-     Machine::enable_interrupts();
+    Machine::enable_interrupts();
 
     /* -- MOST OF WHAT WE NEED IS SETUP. THE KERNEL CAN START. */
 
@@ -308,22 +317,22 @@ int main() {
     /* -- LET'S CREATE SOME THREADS... */
 
     Console::puts("CREATING THREAD 1...\n");
-    char * stack1 = new char[1024];
+    char *stack1 = new char[1024];
     thread1 = new Thread(fun1, stack1, 1024);
     Console::puts("DONE\n");
 
     Console::puts("CREATING THREAD 2...");
-    char * stack2 = new char[1024];
+    char *stack2 = new char[1024];
     thread2 = new Thread(fun2, stack2, 1024);
     Console::puts("DONE\n");
 
     Console::puts("CREATING THREAD 3...");
-    char * stack3 = new char[1024];
+    char *stack3 = new char[1024];
     thread3 = new Thread(fun3, stack3, 1024);
     Console::puts("DONE\n");
 
     Console::puts("CREATING THREAD 4...");
-    char * stack4 = new char[1024];
+    char *stack4 = new char[1024];
     thread4 = new Thread(fun4, stack4, 1024);
     Console::puts("DONE\n");
 
@@ -343,7 +352,7 @@ int main() {
     Thread::dispatch_to(thread1);
 
     /* -- AND ALL THE REST SHOULD FOLLOW ... */
- 
+
     assert(false); /* WE SHOULD NEVER REACH THIS POINT. */
 
     /* -- WE DO THE FOLLOWING TO KEEP THE COMPILER HAPPY. */
